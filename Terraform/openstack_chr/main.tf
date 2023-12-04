02fcdd6bd9ac29da149b76770640caa94f28e0da
Anton Sediuk <a.sediuk@platformaofd.ru>
2023-12-04 16:30:50 +0300

    upd openstack chr

--- Terraform/openstack_chr/main.tf
+++ Terraform/openstack_chr/main.tf
@@ -7,8 +7,8 @@ resource "openstack_compute_keypair_v2" "key_tf" {
 resource "null_resource" "download_and_prepare_image" {
   provisioner "local-exec" {
     command = <<EOT
-      wget -q https://download.mikrotik.com/routeros/${var.chr_ver}/chr-${var.chr_ver}.img.zip
-      unzip chr-${var.chr_ver}.img.zip
+      minc alias set aws https://s3.amazonaws.com $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+      minc cp aws/${S3_BUCKET_NAME}/chr/chr-${var.chr_ver}.img chr-${var.chr_ver}.img
       EOT
   }
 }
